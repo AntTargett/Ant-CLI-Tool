00a1253df361b2d08f3145051b30b7363593e165
Antony Targett <antony.targett@gmail.com>
2018-07-29 15:08:24 +1000

    feat: Changed the break format from ~ to [break]. Updated the index file to have different commands. Updated default command to be the print

--- index.js
+++ index.js
@@ -5,19 +5,35 @@ var program = require("commander");
 var fs = require("fs");
 var path = require("path");
 
-program
-  .option("-s, --set <filepatb>", "The file path to a set of askii art")
-  .action(function() {
-    fs.readFile(path.resolve(__dirname, "ants.txt"), function(err, data) {
-      var ants = data.toString().split("~");
-      var ant = ants[Math.floor(Math.random() * ants.length)];
-      console.log(ant);
-    });
-  })
-  .parse(process.argv);
+program.version("1.5.0")
 
-fs.readFile(path.resolve(__dirname, "ants.txt"), function(err, data) {
-  var ants = data.toString().split("~");
-  var ant = ants[Math.floor(Math.random() * ants.length)];
-  console.log(ant);
+program.command("print").action(function() {
+  fs.readFile(path.resolve(__dirname, "ants.txt"), function(err, data) {
+    var ants = data.toString().split("[break]");
+    var ant = ants[Math.floor(Math.random() * ants.length)];
+    console.log(ant);
+  });
 });
+
+program
+  .command("set <filename>")
+  .description("Sets the file to the profiles")
+  .option("-a, --all", "List all files and folders")
+  .option("-l, --long", "")
+  .action();
+
+program
+  .command("add <file>")
+  .description("Adds a file to the profiles")
+  .option("-a, --all", "List all files and folders")
+  .option("-l, --long", "")
+  .action();
+
+program.parse(process.argv);
+
+if (program.args.length < 1) {
+  fs.readFile(path.resolve(__dirname, "ants.txt"), function(err, data) {
+    var ants = data.toString().split("[break]");
+    var ant = ants[Math.floor(Math.random() * ants.length)];
+    console.log(ant);
+  });}
