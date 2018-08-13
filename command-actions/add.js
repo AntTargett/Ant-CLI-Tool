8d38eaab55c0ae066dbed720723e8a184ea139d0
Antony Targett <antony.targett@gmail.com>
2018-08-13 10:20:29 +1000

    feat: Updated eslint, added eslintrc.

--- command-actions/add.js
+++ command-actions/add.js
@@ -1,19 +1,19 @@
-var fs = require("fs");
-var path = require("path");
+const fs = require("fs");
+const path = require("path");
 
-//The aim of this funciton is to add a path (supplied by user)
+// The aim of this funciton is to add a path (supplied by user)
 // to the main.json file to make it selectable
 function addProfile(filePath) {
-    console.log(filePath);
-    fs.readprofile(path.resolve(filePath), "utf8", function readprofileCallback(
-        err,
-        data
-    ) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(data);
-        }
-    });
+	console.log(filePath);
+	fs.readprofile(path.resolve(filePath), "utf8", (
+		err,
+		data,
+	) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(data);
+		}
+	});
 }
 module.exports = addProfile;
--- command-actions/set.js
+++ command-actions/set.js
@@ -4,30 +4,30 @@ var path = require("path");
 //Changes profile to the profile given
 //Writes to a json object as this application is meant to work without an internet connection
 function setCurrentProfile(profile) {
-    fs.readFile(
-        path.resolve(__dirname, "../", "main.json"),
-        "utf8",
-        function readFileCallback(err, data) {
-            if (err) {
-                console.log(err);
-            } else {
-                obj = JSON.parse(data); //now it an object
-                obj.currentProfile = profile; //add updated profile data
-                json = JSON.stringify(obj); //convert it back to string json
-                fs.writeFile(
-                    path.resolve(__dirname, "../", "main.json"),
-                    json,
-                    "utf8",
-                    function writeFileCallback(err, data) {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log("Successfully swapped profile");
-                        }
-                    }
-                ); 
-            }
-        }
-    );
+	fs.readFile(
+		path.resolve(__dirname, "../", "main.json"),
+		"utf8",
+		function readFileCallback(err, data) {
+			if (err) {
+				console.log(err);
+			} else {
+				var obj = JSON.parse(data); //now it an object
+				obj.currentProfile = profile; //add updated profile data
+				var json = JSON.stringify(obj); //convert it back to string json
+				fs.writeFile(
+					path.resolve(__dirname, "../", "main.json"),
+					json,
+					"utf8",
+					function writeFileCallback(err) {
+						if (err) {
+							console.log(err);
+						} else {
+							console.log("Successfully swapped profile");
+						}
+					}
+				);
+			}
+		}
+	);
 }
 module.exports = setCurrentProfile;
