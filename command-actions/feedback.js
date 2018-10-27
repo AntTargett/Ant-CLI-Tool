21406f8e1100fe65b5f78243f4e8a89790188d36
Antony Targett <antony.targett@gmail.com>
2018-10-27 20:41:18 +1100

    fix: Fix feedback.js and updated eslint

--- command-actions/feedback.js
+++ command-actions/feedback.js
@@ -4,16 +4,15 @@ var path = require("path");
 //Temporary function to print a random string from the feedback file
 //The feedback is not very useful and is directed at a co-worker (sean)
 function printFeedback() {
-	fs.readFile(
-		path.resolve(__dirname, "../askiiArt/", "feedback.txt"),
-		function(err, data) {
-			var feedbacks = data.toString().split("[break]");
-			var feedback =
-                feedbacks[Math.floor(Math.random() * feedbacks.length)];
+	fs.readFile(path.resolve(__dirname, "../askiiArt/", "feedback.txt"), function(
+		err,
+		data
+	) {
+		var feedbacks = data.toString().split("[break]");
+		var feedback = feedbacks[Math.floor(Math.random() * feedbacks.length)];
 
-			console.log(feedback);
-		}
-	);
+		console.log(feedback);
+	});
 }
 
 module.exports = printFeedback;
